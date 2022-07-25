d8198cc9568ca4c78eb094ef9506f1dc36da1d32
Bobby715623 <109953502+Bobby715623@users.noreply.github.com>
2022-07-25 16:29:19 +0700

    Add files via upload

---
+++ demo01.py
@@ -0,0 +1,13 @@
+"""DropDrop"""
+def main():
+    """ไม่รู้จะเขียนอะไรดี"""
+    meow = float (input())
+    if meow >= 2:
+        print ("I'm so happy.")
+    elif meow < 1:
+        print ("I'm so sad.")
+    elif meow < 2:
+        print ("%.2f" %(4-meow))
+main()
+
+# %s string %d integer %.3f float
